797b2de5e3e914c800cc6c92ad1e8185ae50092e
Sean Jackson <PowerUpSoup@gmail.com>
2019-09-01 07:36:05 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
-function instructionsCloseEventListener() {
+function CloseEventListener() {
 
 }
 
-function displayPlantCareInstructions() {
-
-}
 
 function searchResultsEventListener() {
 
 }
 
-function formatSearchParameters() {
+function populateSearchResults(responseJson) {
+    console.log(responseJson);
 
 }
 
-function makeRequestToTrefle() {
+function makeRequestToComicVine(comicVineRequest) {
+        fetch(comicVineRequest)
+        .then(response => response.json())
+        .then(responseJson => populateSearchResults(responseJson))
+        .catch(error => console.log(error))
+    }
 
-}
-
-function populateSearchResults() {
-
-}
-
-function userInputEventListener() {
-    $('form').submit(event => {
-        event.preventDefault();
-        searchInput = document.getElementById("search-by-input").value;
-        formatSearchParameters();
-    });
-}
-
-function plantListEventListener() {
-    
-}
-
-function alphaEventListener() {
-    
+function formatSearchParameters(searchOption, characterEntry) {
+    if (searchOption == "Character") {
+        let comicVineRequest = "https://comicvine.gamespot.com/api/character/?api_key=6f149cf016e46702bc7dac438b4b48106b6a0892&filter=name:" + characterEntry + "&format=json";
+        makeRequestToComicVine(comicVineRequest);
+    } else if (searchOption == "Issue") {
+        let comicVineRequest = "https://comicvine.gamespot.com/api/issues/?api_key=6f149cf016e46702bc7dac438b4b48106b6a0892&filter=name:" + characterEntry + "&format=json";
+        makeRequestToComicVine(comicVineRequest);
+    } else if (searchOption = "Story Arc") {
+        let comicVineRequest = "https://comicvine.gamespot.com/api/story_arcs/?api_key=6f149cf016e46702bc7dac438b4b48106b6a0892&filter=name:" + characterEntry + "&format=json";
+        makeRequestToComicVine(comicVineRequest);
+    }
 }
 
 function watchForm() {
-    
+    $('form').submit(event => {
+        event.preventDefault();
+        let searchOption = document.querySelector('input[name="search-resource"]:checked').value;
+        let characterEntry = document.getElementById("search-by-input").value;
+        console.log(searchOption);
+        console.log(characterEntry);
+        formatSearchParameters(searchOption, characterEntry);        
+    }); 
 }
 
 $(function() {
