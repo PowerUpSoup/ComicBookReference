9317420d9fdb26739daf051331bdd3d77ea46e34
Sean Jackson <PowerUpSoup@gmail.com>
2019-09-03 09:00:06 -0700

    Add files via upload
    Minimum Viable Product nearly complete. 
    Still To Do: Find a way to identify the selected search-item, build the display for the information panel, build the close button for the information panel.

--- index.js
+++ index.js
@@ -2,39 +2,101 @@ function CloseEventListener() {
 
 }
 
-
-function searchResultsEventListener() {
-
+function populateInformationPanel(responseJson, searchOption, identifier) {
+    if (searchOption == "Character") {
+        $('.comic-info-overlay-content').append(
+            `<h2>${responseJson.results[identifier].name}</h2>
+            <img src="${responseJson.results[identifier].image.original_url}>
+            <p><b>Aliases:</b> ${responseJson.results[identifier].aliases}</p>
+            `
+        )
+    } else if (searchOption == "Issue") {
+        $('.comic-info-overlay-content').append(
+            
+            )
+    } else if (searchOption == "Story Arc") {
+        $('.comic-info-overlay-content').append(
+            
+            )
+    }
 }
 
-function populateSearchResults(responseJson) {
+function searchResultsEventListener(responseJson, searchOption) {
+    $('.search-item').on('click', event => {
+        console.log($('.search-item').value);
+        console.log("search-item clicked");
+        console.log(identifier);
+        $('header').addClass("blur");
+        $('#search-options').addClass("blur");
+        $('#comic-search-list').addClass("blur");
+        populateInformationPanel(responseJson, searchOption, identifier);    
+        $('#comic-info-overlay').removeClass("hidden");
+    })
+}
+
+function populateSearchResults(responseJson, searchOption) {
+    $('#search-results').empty();
     console.log(responseJson);
-
+    if (searchOption == "Character") {
+        for (i = 0; i < responseJson.results.length; i++) {
+            $('#search-results').append(
+                `<div class="search-item" id="${i}">
+                    <img src="${responseJson.results[i].image.icon_url}" alt="thumbnail featuring ${responseJson.results[i].name}">
+                <li>
+                    <h3>${responseJson.results[i].name}</h3>
+                    <h4>${responseJson.results[i].real_name}</h4>
+                </li>
+            </div>`
+            )
+        }
+    } else if (searchOption == "Issue") {
+        for (i = 0; i < responseJson.results.length; i++) {
+            $('#search-results').append(
+                `<div class="search-item" id="${i}">
+                    <img src="${responseJson.results[i].image.icon_url}" alt="thumbnail featuring ${responseJson.results[i].name}">
+                <li>
+                    <h3>${responseJson.results[i].name}</h3>
+                    <h4>${responseJson.results[i].volume.name}</h4>
+                </li>
+            </div>`
+            )
+        }
+    } else if (searchOption == "Story Arc") {
+        for (i = 0; i < responseJson.results.length; i++) {
+            $('#search-results').append(
+                `<div class="search-item" id="${i}">
+                    <img src="${responseJson.results[i].image.icon_url}" alt="thumbnail featuring ${responseJson.results[i].name}">
+                <li>
+                    <h3>${responseJson.results[i].name}</h3>
+                </li>
+            </div>`
+            )
+        }
+    }
+$('#search-results').removeClass("hidden");
+searchResultsEventListener(responseJson, searchOption);
 }
 
-function makeRequestToComicVine(comicVineRequest) {
+function makeRequestToComicVine(comicVineRequest, searchOption) {
     console.log("Get Request = " + comicVineRequest);
-    fetch(comicVineRequest, {
-        method: 'GET',
-        headers: {
-            'Access-Control-Allow-Origin:': '*'
-        }
+    fetch(`https://cors-anywhere.herokuapp.com/${comicVineRequest}`, {
+        method: 'GET'
     })
         .then(response => response.json())
-        .then(responseJson => populateSearchResults(responseJson))
+        .then(responseJson => populateSearchResults(responseJson, searchOption))
         .catch(error => console.log(error));
 }
 
 function formatSearchParameters(searchOption, characterEntry) {
     if (searchOption == "Character") {
         let comicVineRequest = "https://comicvine.gamespot.com/api/characters/?api_key=6f149cf016e46702bc7dac438b4b48106b6a0892&filter=name:" + characterEntry + "&format=json";
-        makeRequestToComicVine(comicVineRequest);
+        makeRequestToComicVine(comicVineRequest, searchOption);
     } else if (searchOption == "Issue") {
         let comicVineRequest = "https://comicvine.gamespot.com/api/issues/?api_key=6f149cf016e46702bc7dac438b4b48106b6a0892&filter=name:" + characterEntry + "&format=json";
-        makeRequestToComicVine(comicVineRequest);
+        makeRequestToComicVine(comicVineRequest, searchOption);
     } else if (searchOption = "Story Arc") {
         let comicVineRequest = "https://comicvine.gamespot.com/api/story_arcs/?api_key=6f149cf016e46702bc7dac438b4b48106b6a0892&filter=name:" + characterEntry + "&format=json";
-        makeRequestToComicVine(comicVineRequest);
+        makeRequestToComicVine(comicVineRequest, searchOption);
     }
 }
 
